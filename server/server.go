e98e2950bd8ce2200d27e4554349ab5fbeef08e8
2bit <742005790@qq.com>
2024-06-27 23:46:35 +0800

    feat: CORS

--- server/server.go
+++ server/server.go
@@ -6,6 +6,7 @@ import (
 	U "github.com/ultrazg/xyz/pkg/utils"
 	"github.com/ultrazg/xyz/routes"
 	"log"
+	"net/http"
 )
 
 func Start() {
@@ -16,6 +17,8 @@ func Start() {
 
 	gin.SetMode(gin.ReleaseMode)
 	engine := gin.New()
+
+	engine.Use(Cors())
 
 	routes.RegisterRouters(engine)
 
@@ -33,5 +36,21 @@ func Start() {
 		log.Println("server start fail")
 
 		return
+	}
+}
+
+func Cors() gin.HandlerFunc {
+	return func(context *gin.Context) {
+		method := context.Request.Method
+
+		context.Header("Access-Control-Allow-Origin", "*")
+		context.Header("Access-Control-Allow-Headers", "Content-Type,AccessToken,X-CSRF-Token, Authorization, Token, x-token")
+		context.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PATCH, PUT")
+		context.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Content-Type")
+		context.Header("Access-Control-Allow-Credentials", "true")
+
+		if method == "OPTIONS" {
+			context.AbortWithStatus(http.StatusNoContent)
+		}
 	}
 }
