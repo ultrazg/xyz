22fba559a476ef236aeb8c1a0b4aa0f37043be65
2bit <742005790@qq.com>
2026-07-25 18:18:14 +0800

    fix: 无法获取黑名单列表数据

--- handlers/blocked.go
+++ handlers/blocked.go
@@ -14,33 +14,14 @@ import (
 var BlockedUserLists = func(ctx *gin.Context) {
 	h := ctx.Request.Header
 	XJikeAccessToken := h.Get("x-jike-access-token")
-	now := time.Now()
-	isoTime := now.Format("2006-01-02T15:04:05Z07:00")
 	url := constant.BaseUrl + "/v1/blocked-user/list"
 	headers := map[string]string{
-		"Host":                        "api.xiaoyuzhoufm.com",
-		"User-Agent":                  "Xiaoyuzhou/2.57.1 (build:1576; iOS 17.4.1)",
-		"Market":                      "AppStore",
-		"App-BuildNo":                 "1576",
-		"OS":                          "ios",
-		"x-jike-access-token":         XJikeAccessToken,
-		"Manufacturer":                "Apple",
-		"BundleID":                    "app.podcast.cosmos",
-		"Connection":                  "keep-alive",
-		"abtest-info":                 "{\"old_user_discovery_feed\":\"enable\"}",
-		"Accept-Language":             "zh-Hant-HK;q=1.0, zh-Hans-CN;q=0.9",
-		"Model":                       "iPhone14,2",
-		"app-permissions":             "4",
-		"Accept":                      "*/*",
-		"App-Version":                 "2.57.1",
-		"WifiConnected":               "true",
-		"OS-Version":                  "17.4.1",
-		"x-custom-xiaoyuzhou-app-dev": "",
-		"Local-Time":                  isoTime,
-		"Timezone":                    "Asia/Shanghai",
+		"x-jike-device-id":    utils.DeviceId,
+		"x-jike-access-token": XJikeAccessToken,
+		"content-type":        "application/json",
 	}
-
-	response, code, err := utils.Request(url, http.MethodPost, nil, headers)
+	p := map[string]any{}
+	response, code, err := utils.Request(url, http.MethodPost, p, headers)
 	if err != nil {
 		ctx.JSON(code, gin.H{
 			"code": code,
